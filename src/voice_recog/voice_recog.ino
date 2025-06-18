75c6ccd8aa60537b4a1b2e371ddf6c8ad3c6a172
Sebastian42-5 <sebassotosandrea@gmail.com>
2025-06-18 17:25:00 -0400

    workingh on dfplayer

--- src/voice_recog/voice_recog.ino
+++ src/voice_recog/voice_recog.ino
@@ -1,9 +1,27 @@
 #include <DFRobotDFPlayerMini.h>
 #include <VoiceRecognitionV3.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial MySoftwareSerial(10, 11);
+DFRobotDFPlayerMini myDFPlayer;
 
 void setup() {
   // put your setup code here, to run once:
+  mySoftwareSerial.begin(9600);
+  myDFPlayer.begin(9600);
 
+  if(!myDFPlayer.begin(mySoftwareSerial)){
+    serial.println("Unable to connect to the DFPlayer");
+    serial.println("1. Check connections");
+    serial.println("2. Insert SD card with mp3 file");
+    while(true);
+  }
+
+  serial.prinln("DFPlayer online!")
+
+  myDFPlayer.volume(25);
+  myDFPlayer.play(1);
+  
 }
 
 void loop() {
