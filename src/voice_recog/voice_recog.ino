9659ed610070c4c02f9ed40f8afa7ef3c8ba6cdc
Sebastian42-5 <sebassotosandrea@gmail.com>
2025-07-23 21:27:20 -0400

    adding temp code

--- src/voice_recog/voice_recog.ino
+++ src/voice_recog/voice_recog.ino
@@ -1,40 +1,57 @@
-#include <DFRobotDFPlayerMini.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <U8g2lib.h>
+#include <MUIU8g2.h>
+#include <DHT.h>
+#include <DHT_U.h>
 #include <VoiceRecognitionV3.h>
-#include <SoftwareSerial.h>
+#include <Servo.h>
 
-SoftwareSerial MySoftwareSerial(11, 10);
-SoftwareSerial voiceSerial(12, 13);
 
-DFRobotDFPlayerMini myDFPlayer;
-VR myVR(voiceSerial);
+// DFRobotDFPlayerMini myDFPlayer;
+VR myVR(2, 3);
+Servo myServo;
+int pos = 0;
 
-uint8_t records[7];
-uint8_t buf[64];
+U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
+
+#define DHTPIN 2
+
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE, 6);
+
 
 void setup() {
   // put your setup code here, to run once:
-  MySoftwareSerial.begin(9600);
-  Serial.begin(9600);
-  voiceSerial.begin(9600);
+  // MySoftwareSerial.begin(9600);
+  Serial.begin(115200);
+  myServo.attach(10);
 
-  Serial.println("Trying to connect to the DFPlayer...");
+  dht.begin();
+  u8g2.begin();
 
-  if(!myDFPlayer.begin(MySoftwareSerial, false)){
-    Serial.println("1. Check connections");
-    Serial.println("2. Insert SD card with mp3 file");
-    while(true);
-  }
+  // Serial.println("Trying to connect to the DFPlayer...");
 
-  Serial.println("DFPlayer online!");
+  // if(!myDFPlayer.begin(MySoftwareSerial, false)){
+  //   Serial.println("1. Check connections");
+  //   Serial.println("2. Insert SD card with mp3 file");
+  //   while(true);
+  // }
 
-  int fileCount = myDFPlayer.readFileCounts();
-  Serial.print("Files on SD card: ");
-  Serial.println(fileCount);
+  // Serial.println("DFPlayer online!");
 
-  myDFPlayer.EQ(DFPLAYER_EQ_NORMAL);
-  myDFPlayer.volume(25);
+  // int fileCount = myDFPlayer.readFileCounts();
+  // Serial.print("Files on SD card: ");
+  // Serial.println(fileCount);
+
+  // myDFPlayer.EQ(DFPLAYER_EQ_NORMAL);
+  // myDFPlayer.volume(25);
+
+  myVR.begin(9600);
+  Serial.println("VoiceRecognitionModule initialized.");
   
-  if (myVR.begin()) {
+  if (myVR.clear() == 0) {
     Serial.println("VoiceRecognitionModule ready.");
   } else {
     Serial.println("VoiceRecognitionModule not detected.");
@@ -42,7 +59,7 @@ void setup() {
   }
 
   // Load command from index 2 (adjust if needed)
-  if (myVR.load((uint8_t)0, 1) >= 0) {
+  if (myVR.load((uint8_t)0) >= 0) {
     Serial.println("VR Module: Command loaded.");
   } else {
     Serial.println("VR Module: Load failed.");
@@ -52,15 +69,55 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  uint8_t records[7];
+  uint8_t buf[64];
+
   int ret = myVR.recognize(buf, 50);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
   if(ret > 0){
     uint8_t command = buf[1];
     Serial.println("Recognized command index: ");
     Serial.println(command);
 
-    if(command == 2){
-      Serial.println("Playing 001.mp3")
-      myDFPlayer.play(1);
+    if(command == 0){
+      Serial.println("Recieved");
+      // Serial.println("Playing 001.mp3");
+      // myDFPlayer.play(1);
+      for(int pos = 0; pos <= 30; pos+=2){
+        myServo.write(pos);
+        delay(40);
+      }
+      for(int pos = 30; pos > 0; pos-=2){
+        myServo.write(pos);
+        delay(40);
+      }
+
+      if(isnan(h) || isnan(t)){
+        u8g2.clearBuffer();
+        u8g2.setFont(u8g2_font_6x10_tf);
+        u8g2.drawStr(10, 30, "Sensor error!");
+        u8g2.sendBuffer();
+        delay(2000);
+        return;
+      }
+
+      char tempBuffer[16];
+      char humBuffer[16];
+
+      snprintf(tempBuffer, sizeof(tempBuffer), "Temp: %.1f C", t);
+      snprintf(humBuffer, sizeof(humBuffer), "Hum:  %.1f %%", h);
+
+      u8g2.firstPage();
+      do{             
+        u8g2.setFont(u8g2_font_6x10_tf);  
+        u8g2.drawStr(10, 20, tempBuffer);    
+        u8g2.drawStr(10, 40, humBuffer);                      
+      }while (u8g2.nextPage());
+      delay(2000);
     }
   }
 }
